9e1d9728bbba3d6ef1fdd01c86fb570bbe4faaa5
Ali Reza <alirezayahya@gmail.com>
2018-08-22 10:01:17 +0200

    Update handler.go

--- handler/handler.go
+++ handler/handler.go
@@ -14,9 +14,9 @@ type Handler struct {
 func New(r resource.Manager) func(s *godog.Suite) {
 	h := &Handler{r}
 	return func(s *godog.Suite) {
-		s.Step(`^"([^"]*)" send request to "([^"]*)"`, h.sendRequestTo)
+		s.Step(`^"([^"]*)" send request to "([^"]*)"$`, h.sendRequestTo)
 		s.Step(`^"([^"]*)" send request to "([^"]*)" with body$`, h.sendRequestToWithBody)
-		s.Step(`^"([^"]*)" response code should be (\d+)`, h.responseCodeShouldBe)
+		s.Step(`^"([^"]*)" response code should be (\d+)$`, h.responseCodeShouldBe)
 		s.Step(`^"([^"]*)" response body should be$`, h.responseBodyShouldBe)
 		s.Step(`^set "([^"]*)" response code to (\d+) and response body$`, h.setResponseCodeToAndResponseBody)
 		s.Step(`^set "([^"]*)" with path "([^"]*)" response code to (\d+) and response body$`, h.setWithPathResponseCodeToAndResponseBody)
@@ -24,8 +24,8 @@ func New(r resource.Manager) func(s *godog.Suite) {
 		s.Step(`^"([^"]*)" table "([^"]*)" should look like$`, h.tableShouldLookLike)
 		s.Step(`^set "([^"]*)" table "([^"]*)" to empty`, h.setTableToEmpty)
 		s.Step(`^publish message to "([^"]*)" target "([^"]*)" with payload$`, h.publishMessageToTargetWithPayload)
-		s.Step(`^listen message from "([^"]*)" target "([^"]*)"`, h.listenMessageFromTarget)
-		s.Step(`^message from "([^"]*)" target "([^"]*)" count should be (\d+)`, h.messageFromTargetCountShouldBe)
+		s.Step(`^listen message from "([^"]*)" target "([^"]*)"$`, h.listenMessageFromTarget)
+		s.Step(`^message from "([^"]*)" target "([^"]*)" count should be (\d+)$`, h.messageFromTargetCountShouldBe)
 		s.Step(`^message from "([^"]*)" target "([^"]*)" should look like$`, h.messageFromTargetShouldLookLike)
 
     }
