65a7469db7df20a6e90a7f6d9b56945f88ee1507
alileza <alirezayahya@gmail.com>
2018-08-22 12:22:12 +0200

    add generate package

---
+++ generate/handler/handler.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/alileza/tomato/dictionary"
+)
+
+const (
+	handlerTmpl = `/* GENERATED FILE - DO NOT EDIT */
+/* Rebuild from the cmd/gen/main.go tool */
+package handler
+
+import (
+	"github.com/DATA-DOG/godog"
+	"github.com/alileza/tomato/resource"
+)
+
+type Handler struct {
+	resource resource.Manager
+}
+
+func New(r resource.Manager) func(s *godog.Suite) {
+	h := &Handler{r}
+	return func(s *godog.Suite) {
+%s
+    }
+}`
+)
+
+func step(expr, handle string) string {
+	return fmt.Sprintf("\t\ts.Step(`^%s`, h.%s)", expr, handle)
+}
+
+func Generate(dict *dictionary.Dictionary) (*bytes.Buffer, error) {
+	steps := bytes.NewBuffer(nil)
+	for _, resource := range dict.Resources.List {
+		for _, action := range resource.Actions {
+			for _, expr := range action.Expr() {
+				fmt.Fprintf(steps, step(expr, action.Handle)+"\n")
+			}
+		}
+	}
+	return bytes.NewBufferString(fmt.Sprintf(handlerTmpl, steps.String())), nil
+}
