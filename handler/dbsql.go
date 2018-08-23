dfd1580fd0647e1b787a148b5589fa8962040662
Stefan Naglee <stefannaglee@gmail.com>
2018-08-23 09:45:46 +0200

    WIP: Change comparison operators to use gherkin.row

--- handler/dbsql.go
+++ handler/dbsql.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/DATA-DOG/godog/gherkin"
 	"github.com/alileza/tomato/resource/db/sql"
-	"github.com/alileza/tomato/util/conv"
 )
 
 func (h *Handler) getResourceDB(name string) sql.SQL {
@@ -26,7 +25,7 @@ func (h *Handler) setTableToEmpty(name, tables string) error {
 			return dbClient.TruncateAll()
 		}
 
-		if err := dbClient.Set(table, make([]map[string]string, 0)); err != nil {
+		if err := dbClient.Set(table, &gherkin.DataTable{}); err != nil {
 			return err
 		}
 	}
@@ -34,34 +33,22 @@ func (h *Handler) setTableToEmpty(name, tables string) error {
 	return nil
 }
 func (h *Handler) setTableListOfContent(name, table string, content *gherkin.DataTable) error {
-	dbClient := h.getResourceDB(name)
-
-	rows, err := conv.GherkinTableToSliceOfMap(content)
-	if err != nil {
-		return err
-	}
-
-	return dbClient.Set(table, rows)
+	return h.getResourceDB(name).Set(table, content)
 }
 
 func (h *Handler) tableShouldLookLike(name, tableName string, content *gherkin.DataTable) error {
 	dbClient := h.getResourceDB(name)
-
-	rows, err := conv.GherkinTableToSliceOfMap(content)
-	if err != nil {
-		return err
-	}
 
 	rowsCount, err := dbClient.Count(tableName, nil)
 	if err != nil {
 		return err
 	}
 
-	if rowsCount != len(rows) {
-		return fmt.Errorf("expecting row count to be %d, got %d", len(rows), rowsCount)
+	if rowsCount != len(content.Rows) {
+		return fmt.Errorf("expecting row count to be %d, got %d", len(content.Rows), rowsCount)
 	}
 
-	for _, row := range rows {
+	for _, row := range content.Rows {
 		count, err := dbClient.Count(tableName, row)
 		if err != nil {
 			return err
