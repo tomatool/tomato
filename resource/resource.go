8cf12be7717069c5b4aac864723e0854848f1a85
alileza <alirezayahya@gmail.com>
2018-09-21 15:29:55 +0200

    add check resource before executing test
    add wait resource before execution
    
    even if new is error

--- resource/resource.go
+++ resource/resource.go
@@ -1,6 +1,9 @@
 package resource
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/alileza/tomato/config"
 	"github.com/pkg/errors"
 )
@@ -59,4 +62,47 @@ func (m *Manager) Reset() {
 		}
 		rr.Reset()
 	}
+}
+
+func (m *Manager) Ready() error {
+	for _, r := range m.resources {
+		if !r.config.ReadyCheck {
+			continue
+		}
+
+		var (
+			rr  Resource
+			err error
+		)
+
+		for i := 0; i < 30; i++ {
+			switch r.config.Type {
+			case "database/sql":
+				rr, err = m.GetDatabaseSQL(r.config.Name)
+			case "queue":
+				rr, err = m.GetQueue(r.config.Name)
+			case "http/server":
+				rr, err = m.GetHTTPServer(r.config.Name)
+			case "http/client":
+				rr, err = m.GetHTTPClient(r.config.Name)
+			default:
+				err = errors.New("he")
+			}
+			if err != nil {
+				fmt.Printf("%d [%s] not yet ready : %v \n%+v\n", (i + 1), r.config.Name, err, r.config.Params)
+				return err
+			}
+
+			if err = rr.Ready(); err != nil {
+				fmt.Printf("%d [%s] not yet ready : %v \n%+v\n", (i + 1), r.config.Name, err, r.config.Params)
+				time.Sleep(time.Second)
+				continue
+			}
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
