14aa9fa8edc66bb36b88c1b934b7ef8d1f84fcfb
alileza <alirezayahya@gmail.com>
2018-09-27 18:47:15 +0200

    update documentation template

--- generate/docs/tmpl_html.go
+++ generate/docs/tmpl_html.go
@@ -36,7 +36,7 @@ resources:
 	  {{range .Options}}{{.Name}}: # ({{.Type}}) {{.Description}}
 	  {{end}}</code></pre>
 					</div>
-					
+
 					<h6>Actions</h6>
 					<ul class="list-unstyled">
 					{{range .Actions}}
@@ -48,9 +48,9 @@ resources:
 					  </li>
 					  {{end}}
 					</ul>
-					
-					
-					
+
+
+
 					<hr>
                     {{end}}
 				</div>
@@ -59,7 +59,7 @@ resources:
 	</div>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    
+
 
 	<script type="text/javascript">
         $('body').scrollspy({
