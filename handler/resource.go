6f7ffc3d8266eceb2db15a075ece79d170f3b65f
Ali Reza <alirezayahya@gmail.com>
2018-12-21 16:50:48 +0800

    update handler to use type as resource name instead

--- handler/resource.go
+++ handler/resource.go
@@ -5,81 +5,74 @@ import (
 
 	"github.com/tomatool/tomato/config"
 	"github.com/tomatool/tomato/handler/database/sql"
+	"github.com/tomatool/tomato/handler/http/client"
+	"github.com/tomatool/tomato/handler/http/server"
 	"github.com/tomatool/tomato/handler/queue"
-	"github.com/tomatool/tomato/resource/database/sql/mysql"
-	"github.com/tomatool/tomato/resource/database/sql/postgres"
-	"github.com/tomatool/tomato/resource/http/client"
-	"github.com/tomatool/tomato/resource/http/server"
-	"github.com/tomatool/tomato/resource/queue/nsq"
-	"github.com/tomatool/tomato/resource/queue/rabbitmq"
-	"github.com/tomatool/tomato/resource/shell"
+	"github.com/tomatool/tomato/handler/shell"
+	"github.com/tomatool/tomato/resource"
+	mysql_r "github.com/tomatool/tomato/resource/database/sql/mysql"
+	postgres_r "github.com/tomatool/tomato/resource/database/sql/postgres"
+	httpclient_r "github.com/tomatool/tomato/resource/http/client"
+	httpserver_r "github.com/tomatool/tomato/resource/http/server"
+	nsq_r "github.com/tomatool/tomato/resource/queue/nsq"
+	rabbitmq_r "github.com/tomatool/tomato/resource/queue/rabbitmq"
+	shell_r "github.com/tomatool/tomato/resource/shell"
 )
 
-func (h *Handler) Register(name string, cfg *config.Resource) error {
-	var (
-		err error
-	)
+var resources = map[string]string{
+	"httpclient": "http/client",
+	"httpserver": "http/server",
+	"postgres":   "database/sql",
+	"mysql":      "database/sql",
+	"rabbitmq":   "queue",
+	"nsq":        "queue",
+	"shell":      "shell",
+}
 
-	resources := map[string]string{
-		"httpclient": "http/client",
-		"httpserver": "http/server",
-		"postgres":   "database/sql",
-		"mysql":      "database/sql",
-		"rabbitmq":   "queue",
-		"nsq":        "queue",
+func createResource(cfg *config.Resource) (resource.Resource, error) {
+	switch cfg.Type {
+	case "mysql":
+		return mysql_r.New(cfg)
+	case "postgres":
+		return postgres_r.New(cfg)
+	case "rabbitmq":
+		return rabbitmq_r.New(cfg)
+	case "nsq":
+		return nsq_r.New(cfg)
+	case "httpclient":
+		return httpclient_r.New(cfg)
+	case "httpserver":
+		return httpserver_r.New(cfg)
+	case "shell":
+		return shell_r.New(cfg)
 	}
+	return nil, fmt.Errorf("resource %s not found", cfg.Type)
+}
+
+func (h *Handler) Register(name string, cfg *config.Resource) error {
+	r, err := createResource(cfg)
+	if err != nil {
+		return err
+	}
+	h.resources[cfg.Name] = r
 
 	switch resources[cfg.Type] {
 	case "database/sql":
-		h.sqlDatabases[cfg.Name], err = createSQL(cfg)
-		h.resources[cfg.Name] = h.sqlDatabases[cfg.Name]
+		h.sqlDatabases[cfg.Name] = r.(sql.Resource)
 	case "shell":
-		h.shells[cfg.Name], err = shell.New(cfg)
-		h.resources[cfg.Name] = h.shells[cfg.Name]
+		h.shells[cfg.Name] = r.(shell.Resource)
 	case "http/client":
-		h.httpClients[cfg.Name], err = client.New(cfg)
-		h.resources[cfg.Name] = h.httpClients[cfg.Name]
+		h.httpClients[cfg.Name] = r.(client.Resource)
 	case "http/server":
-		h.httpServers[cfg.Name], err = server.New(cfg)
-		h.resources[cfg.Name] = h.httpServers[cfg.Name]
+		h.httpServers[cfg.Name] = r.(server.Resource)
 	case "queue":
-		h.queues[cfg.Name], err = createQueue(cfg)
-		h.resources[cfg.Name] = h.queues[cfg.Name]
+		h.queues[cfg.Name] = r.(queue.Resource)
 	}
 	if err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func createSQL(cfg *config.Resource) (sql.Resource, error) {
-	driver, ok := cfg.Params["driver"]
-	if !ok {
-		return nil, fmt.Errorf("params driver is required for database/sql resource")
-	}
-
-	switch driver {
-	case "mysql":
-		return mysql.New(cfg)
-	case "postgres":
-		return postgres.New(cfg)
-	}
-	return nil, fmt.Errorf("%s is not registered driver for database/sql", driver)
-}
-
-func createQueue(cfg *config.Resource) (queue.Resource, error) {
-	driver, ok := cfg.Params["driver"]
-	if !ok {
-		return nil, fmt.Errorf("params driver is required for queue resource")
-	}
-	switch driver {
-	case "rabbitmq":
-		return rabbitmq.New(cfg)
-	case "nsq":
-		return nsq.New(cfg)
-	}
-	return nil, fmt.Errorf("%s is not registered driver for queue", driver)
 }
 
 func (h *Handler) reset() {
