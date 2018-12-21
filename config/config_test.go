76f65bb65987a755c188f837674193a7c0e023f8
Ali Reza <alirezayahya@gmail.com>
2018-12-21 14:45:40 +0800

    add tests for config

---
+++ config/config_test.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRetrieve(t *testing.T) {
+	cfg, err := Retrieve("testdata/conf.good.yml")
+	assert.NoError(t, err)
+
+	assert.Equal(t, len(cfg.Resources), 7)
+	assert.Equal(t, cfg.StopOnFailure, false)
+}
+
+func TestEnv(t *testing.T) {
+	os.Clearenv()
+	timeNow := time.Now().Format(time.RFC3339)
+	os.Setenv("samplevar", timeNow)
+	os.Setenv("STOP_ON_FAILURE", "true")
+
+	cfg, err := Retrieve("testdata/env.good.yml")
+	assert.NoError(t, err)
+
+	assert.Equal(t, cfg.StopOnFailure, true)
+	assert.Equal(t, cfg.Resources[0].Params["datasource"], timeNow)
+}
