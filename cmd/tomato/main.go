dbc4ee604e049f946fe7753cc73812bd0a3150a3
Stefan Naglee <stefannaglee@gmail.com>
2019-03-19 11:45:38 +0100

    Add env file flag

--- cmd/tomato/main.go
+++ cmd/tomato/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/DATA-DOG/godog"
 	"github.com/DATA-DOG/godog/colors"
+	"github.com/joho/godotenv"
 	"github.com/pkg/errors"
 	"github.com/tomatool/tomato/config"
 	"github.com/tomatool/tomato/formatter"
@@ -20,6 +21,7 @@ import (
 var (
 	configFile       string
 	featuresPath     string
+	envFile          string
 	resourcesTimeout time.Duration
 	resourcesCheck   bool
 )
@@ -31,12 +33,19 @@ func main() {
 
 	app.Flag("config.file", "tomato configuration file path.").Short('c').Default("tomato.yml").StringVar(&configFile)
 	app.Flag("features.path", "tomato features folder path.").Short('f').Default("features").StringVar(&featuresPath)
+	app.Flag("env.file", "environment file path.").Short('e').StringVar(&envFile)
 	app.Flag("resources.timeout", "tomato will automatically wait for resource to be ready, and at some out it giving up.").Short('t').Default("30s").DurationVar(&resourcesTimeout)
 	app.Flag("resources.check", "tomato only check if the resources is all ready, and exit without executing the tests.").Short('e').Default("false").BoolVar(&resourcesCheck)
 
 	_, err := app.Parse(os.Args[1:])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Error parsing flag"))
+		os.Exit(1)
+	}
+
+	err = godotenv.Load(envFile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Error loading env file"))
 		os.Exit(1)
 	}
 
