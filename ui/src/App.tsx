03ffa8a46de2818d7da81b6bc7529c0341272b19
Ali Reza Yahya <alirezayahya@gmail.com>
2020-07-11 02:23:25 +0200

    add ui

---
+++ ui/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Config from './Config';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Config />
+    </div>
+  );
+}
+
+export default App;
