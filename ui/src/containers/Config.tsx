541d98895d10081e6bbd6c2134249d2e5cca974a
Ali Reza Yahya <alirezayahya@gmail.com>
2020-08-02 12:56:43 +0200

    ui: update css and edit server

--- ui/src/containers/Config.tsx
+++ ui/src/containers/Config.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { Input, List, Button } from 'antd';
+import React, { useState } from 'react';
+import { Input, List, Button, Form } from 'antd';
+import { PlusOutlined, CloseCircleTwoTone } from '@ant-design/icons';
 import ConfigResourceContainer from './ConfigResource'
 import { IDictionary, IResource, IConfig } from '../interfaces'
-import { getResourceParams } from '../dictionary'
 
 interface IProps {
     dictionary: IDictionary;
@@ -12,9 +12,37 @@ interface IProps {
 function ConfigContainer({ dictionary, config }:IProps) {
   const [configState, setConfig] = useState<IConfig>(config);
 
-  let handleFeaturesPathChange = () => {}
+  const handleFeaturePathsChange = (index) => { 
+    return (e) => {
+      let featurePaths = configState.features_path;
+      featurePaths[index] = e.target.value;
+  
+      setConfig({
+        features_path: configState.features_path.map((val, i) => {
+          if (i === index) return e.target.value;
+          return val;
+        }),
+        resources: configState.resources
+      })
+    }
+  }
+  const handleFeaturePathsDelete = (index) => {
+    return () => {
+      setConfig({
+        features_path: configState.features_path.filter((item, i) => index !== i),
+        resources: configState.resources
+      })
+    }
+  }
+  const handleFeaturePathsAdd = () => {
+    setConfig({
+      features_path: [...configState.features_path, `somewhere/features-${configState.features_path.length}`],
+      resources: configState.resources
+    })
+  }
 
-  let handleResourceItemChange = (selectedName: string, newItem: IResource) => {
+  const handleResourceItemChange = (selectedName: string, newItem: IResource | null) => {
+    if (newItem === null) return handleResourceItemRemove(selectedName);
     const newResourceItem = configState.resources.map((item: IResource) => {
         if (item.name === selectedName) {
             return newItem;
@@ -28,34 +56,30 @@ function ConfigContainer({ dictionary, config }:IProps) {
     });
   }
 
-  let handleNewResourceItem = () => {
+  const handleResourceItemRemove = (selectedName: string) => {
+    const newResourceItems = configState.resources.filter((item: IResource) => {
+        return (item.name !== selectedName)
+    });
+    
+    setConfig({
+        features_path: configState.features_path,
+        resources: newResourceItems
+    });
+  }
+
+  const handleAddResourceItem = () => {
     const newResourceItem = {
-        name: "new",
-        type: "httpclient",
+        name: `new-${configState.resources.length}`,
+        type: "wiremock",
         parameters: {}
     }; 
-
-    getResourceParams(dictionary, "httpclient").forEach((param) => {
-      newResourceItem.parameters[param.name] = ""
-    }) 
-    
-    console.log(newResourceItem)
-
+      
     setConfig({
         features_path: configState.features_path,
         resources: [...configState.resources, newResourceItem]
     });
   }
 
-//   useEffect(() => 
-
-//   ,[configState])
-
-  
-
-  let validateConfig = (config: IConfig) => {
-      // validate & return error
-  }
 
   const handleSave = () => {
       
@@ -63,34 +87,62 @@ function ConfigContainer({ dictionary, config }:IProps) {
   
   return (
     <div className="App">
-        <label htmlFor="">Features Path</label>
-        <List
-            itemLayout="horizontal"
-            dataSource={configState.features_path}
-            renderItem={item => (
-            <List.Item>
-                <Input placeholder="Features path" value={item}/>
-            </List.Item>
-            )}
-        />
-
-        <label htmlFor="">Resources</label>
-        <List
-            itemLayout="horizontal"
-            dataSource={configState.resources}
-            renderItem={item => (
-            <List.Item>
-                <ConfigResourceContainer 
-                    dictionary={dictionary} 
-                    item={item} 
-                    handleResourceItemChange={handleResourceItemChange} 
-                />          
-            </List.Item>
-            )}
-        />
-        <Button onClick={handleNewResourceItem} type="primary">
-          New Resource
-        </Button>
+        <h1>Edit <span role="img" aria-label="tomato">🍅</span> Config</h1>
+        <div>
+          <strong>Features Path</strong>
+          <List
+              itemLayout="horizontal"
+              dataSource={configState.features_path}
+              renderItem={(item, index) => (
+              <List.Item>
+                <Input 
+                  style={{ width: '300px' }}
+                  onChange={handleFeaturePathsChange(index)} 
+                  placeholder="Features path" 
+                  value={item} 
+                  addonAfter={
+                    <CloseCircleTwoTone name={item} onClick={handleFeaturePathsDelete(index)} />}
+                />
+              </List.Item>
+              )}
+          />
+          <Button
+              style={{ height: '30px' }}
+              onClick={handleFeaturePathsAdd}
+            >
+              <PlusOutlined /> 
+          </Button>
+        </div>
+        <table style={{ width: '100%' }}>
+          <tr>
+            <th>Resources</th>
+          </tr>
+          {configState.resources.map((item, index) => {
+            return (
+              <ConfigResourceContainer 
+                        key={index}
+                        dictionary={dictionary} 
+                        item={item} 
+                        handleResourceItemChange={handleResourceItemChange} 
+                        />
+            );
+          })}
+          <tr>
+            <td colSpan={3}>
+              <Form.Item>
+                <Button
+                  style={{ height: '60px' }}
+                  type="dashed"
+                  onClick={handleAddResourceItem}
+                  block
+                >
+                  <PlusOutlined /> Add new resource
+                </Button>
+              </Form.Item>
+            </td>
+          </tr>
+        </table>
+          
         <Button onClick={handleSave} type="primary">
           Save
         </Button>
