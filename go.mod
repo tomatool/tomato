50aea29924597a030db28a8787c3bc6157c36b5e
Ali Reza Yahya <alirezayahya@gmail.com>
2020-08-05 22:31:50 +0200

    update editor

--- go.mod
+++ go.mod
@@ -7,15 +7,15 @@ require (
 	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc
 	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
 	github.com/bitly/go-nsq v1.0.7
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/ghodss/yaml v1.0.0
 	github.com/go-sql-driver/mysql v1.4.1
+	github.com/gobuffalo/packr v1.30.1
 	github.com/golang/mock v1.4.3
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/joho/godotenv v1.3.0
 	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lib/pq v1.0.0
 	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/mattn/go-runewidth v0.0.4 // indirect
@@ -32,10 +32,7 @@ require (
 	github.com/yudai/gojsondiff v1.0.0
 	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
 	github.com/yudai/pp v2.0.1+incompatible // indirect
-	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
-	golang.org/x/sys v0.0.0-20190515120540-06a5c4944438 // indirect
 	google.golang.org/appengine v1.5.0 // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.2.2
 )
---
+++ web/web.go
@@ -0,0 +1,222 @@
+package web
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/DATA-DOG/godog/colors"
+	"github.com/ghodss/yaml"
+	"github.com/gobuffalo/packr"
+	"github.com/oklog/run"
+	"github.com/urfave/cli"
+	stdyaml "gopkg.in/yaml.v2"
+)
+
+type Web struct {
+}
+
+func New() *Web {
+	return &Web{}
+}
+
+func openbrowser(url string) (err error) {
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	return
+}
+
+func yamlToJSON(y []byte) (map[string]interface{}, error) {
+	by, err := yaml.YAMLToJSON(y)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(map[string]interface{})
+	if err := json.Unmarshal(by, &result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func jsonToYaml(y []byte) (map[string]interface{}, error) {
+	by, err := yaml.JSONToYAML(y)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(map[string]interface{})
+	if err := yaml.Unmarshal(by, &result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func (w *Web) Handler(ctx *cli.Context) error {
+	var g run.Group
+	var configPath string
+	if len(ctx.Args()) == 1 {
+		configPath = ctx.Args()[0]
+	}
+	if configPath == "" {
+		return errors.New("This command takes one argument: <config path>\nFor additional help try 'tomato edit -help'")
+	}
+
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return err
+	}
+	printErr := func(err error) {
+		l.Close()
+		if err != nil {
+			log.Printf(colors.Bold(colors.Red)("ERR: %v"), err)
+		}
+	}
+	g.Add(func() error {
+
+		if err := openbrowser("http://" + l.Addr().String() + "/index.html"); err != nil {
+			return err
+		}
+
+		box := packr.NewBox("..")
+
+		return http.Serve(l, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			switch r.Method {
+			case http.MethodGet:
+				handleGet(box, configPath, l)(w, r)
+			case http.MethodPost:
+				handlePost(configPath)(w, r)
+			default:
+				http.Error(w, "", http.StatusMethodNotAllowed)
+			}
+		}))
+	}, printErr)
+
+	term := make(chan os.Signal, 1)
+	signal.Notify(term, os.Interrupt, syscall.SIGTERM)
+	g.Add(func() error {
+		select {
+		case <-term:
+			log.Printf(colors.Bold(colors.Yellow)("Received SIGTERM, exiting gracefully..."))
+		}
+		return nil
+	}, printErr)
+
+	return g.Run()
+}
+
+func handleGet(box packr.Box, configPath string, l net.Listener) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("client") != "" {
+			setupResponse(&w, r)
+			switch r.Method {
+			case http.MethodGet:
+				dict, err := box.Find("./dictionary.yml")
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				dictMap, err := yamlToJSON(dict)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				cfg, err := ioutil.ReadFile(configPath)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				config, err := yamlToJSON(cfg)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+
+				b, err := json.Marshal(map[string]interface{}{
+					"config":     config,
+					"dictionary": dictMap,
+				})
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				w.Header().Set("Content-Type", "application/json")
+				w.Write(b)
+				return
+			}
+		} else {
+			if r.URL.Path == "/" {
+				index, err := box.Find("./ui/build/index.html")
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				var buf bytes.Buffer
+
+				t := template.Must(template.New("config").Parse(string(index)))
+				if err := t.ExecuteTemplate(&buf, "config", map[string]string{"ServerURL": "http://" + l.Addr().String()}); err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+
+				w.Write(index)
+				return
+			}
+
+			fs := http.FileServer(http.Dir("./ui/build"))
+			fs.ServeHTTP(w, r)
+		}
+	}
+}
+
+func handlePost(configPath string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Printf("%s\n", b)
+		res, err := jsonToYaml(b)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		out, err := stdyaml.Marshal(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if err := ioutil.WriteFile(configPath, out, 0755); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
