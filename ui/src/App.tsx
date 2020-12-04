7fb5c1cc77bee8b6c038b9defa15d9e211b9f748
alileza <alirezayahya@gmail.com>
2020-12-04 22:55:37 +0100

    refactor app

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,40 +4,28 @@ import ConfigView from './ConfigView';
 import { Config, Dictionary } from './interfaces';
 
 
+
 function App() {
   const [config, setConfig] = useState({} as Config);
   const [dictionary, setDictionary] = useState({} as Dictionary);
-  const [loading, setLoading] = useState(0 as number);
+  // this number should represent how many http call required for the loading to be exit
+  const [loading, setLoading] = useState(2 as number);
+
+  async function fetchStateFromRemote(path: string, setter: Function) {
+    const { data }  = await axios.get(path);
+    setter(data);
+    setLoading((l) => l-1);
+  }
 
   useEffect(() => {
-    const initConfig = async () => {
-      try {
-        const response = await axios.get('/api/config');
-        setConfig(response.data);
-        setLoading(currentState => currentState+1);
-      } catch (e) {
-        alert(e);
-      }
-    };
-    initConfig();
+    fetchStateFromRemote('/api/config', setConfig);
+    fetchStateFromRemote('/api/dictionary', setDictionary);
   },[]);
 
-  useEffect(() => {
-    const initDictionary = async () => {
-      try {
-        const response = await axios.get('/api/dictionary');
-        setDictionary(response.data);
-        setLoading(currentState => currentState+1);
-      } catch (e) {
-        alert(e);
-      }
-    };
-    initDictionary();
-  },[]);
-  
+ 
   return (
     <div className="App">
-        {loading < 2 ? `(${loading}) loading...` : <ConfigView config={config} dictionary={dictionary} /> }
+        {loading !== 0 ? `(${loading}) loading...` : <ConfigView config={config} dictionary={dictionary} /> }
     </div>
   );
 }
